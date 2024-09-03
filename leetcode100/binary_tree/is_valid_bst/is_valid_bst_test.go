ef54d2bff5e9a64529b351dea0b29980be3995c8
szuwgh <895340293@qq.com>
2024-09-03 20:12:33 +0800

    save

--- leetcode100/binary_tree/is_valid_bst/is_valid_bst_test.go
+++ leetcode100/binary_tree/is_valid_bst/is_valid_bst_test.go
@@ -1,1 +1,15 @@
 package is_valid_bst
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsValidBst(t *testing.T) {
+	// 示例用法
+	root1 := &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}
+	root2 := &TreeNode{Val: 5, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 6}}}
+
+	fmt.Println(isValidBST(root1)) // 输出: true
+	fmt.Println(isValidBST(root2)) // 输出: false
+}
